05997f89c49320410d2f71f6c66037e2f5312cff
daohoangson123 <daohoangson2109>
2023-01-21 15:27:10 +0700

    search update

--- src/components/Header/Mobilemenu.jsx
+++ src/components/Header/Mobilemenu.jsx
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 export default function Mobilemenu() {
   return (
     <div>
-        <ul className=' absolute top-[104px] right-0 grid gap-3 bg-black bg-opacity-90 sm:hidden '>
-                <li className=' px-5 hover:bg-white hover:text-black '>
+        <ul className=' absolute top-[72px] right-0 grid bg-black bg-opacity-90 sm:hidden '>
+                <li className=' px-5 p-2 hover:bg-white hover:text-black '>
                     <Link to='/'>Home</Link>
                 </li>
-                <li className=' px-5 hover:bg-white hover:text-black '>
+                <li className=' px-5 p-2 hover:bg-white hover:text-black '>
                     <Link to='/trending'>Trending</Link>
                 </li>
-                <li className=' px-5 hover:bg-white hover:text-black '>
+                <li className=' px-5 p-2 hover:bg-white hover:text-black '>
                     <Link to='/movies'>Movies</Link>
                 </li>
-                <li className=' px-5 hover:bg-white hover:text-black '>
+                <li className=' px-5 p-2 hover:bg-white hover:text-black '>
                     <Link to='/tvseries'>TV Series</Link>
                 </li>
-                <button type='button' className=' px-5 hover:bg-white hover:text-black '>
+                <button type='button' className=' px-5 p-2 hover:bg-white hover:text-black '>
                     Search
                 </button>
             </ul>
